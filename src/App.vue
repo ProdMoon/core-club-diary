44a8151d12f04b4e2757b9fcc439c13001427adc
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2024-02-24 03:57:49 +0900

    fix: 하단바 아이콘으로 변경

--- src/App.vue
+++ src/App.vue
@@ -28,10 +28,22 @@ const route = useRoute();
       <RouterView />
       <footer v-if="route.path !== '/login'" class="absolute bottom-0 z-30 w-full h-20 bg-pink-50 rounded-t-2xl border-t border-pink-200">
         <nav class="w-full h-full flex items-center justify-around text-pink-300">
-          <router-link to="/home">일지</router-link>
-          <router-link to="/badge">배지</router-link>
-          <router-link to="/beaurit-zone">뷰릿존</router-link>
-          <router-link to="/settings">설정</router-link>
+          <router-link to="/home" class="flex flex-col items-center justify-center">
+            <img :src="route.path === '/home' ? '/footer/calendar-gray.png' : '/footer/calendar-pink.png'" class="w-7 h-7" alt="calendar" />
+            <span :class="`${route.path === '/home' ? 'text-gray-400' : ''} mt-1 text-xs`">일지</span>
+          </router-link>
+          <router-link to="/badge" class="flex flex-col items-center justify-center">
+            <img :src="route.path === '/badge' ? '/footer/badge-gray.png' : '/footer/badge-pink.png'" class="w-7 h-7" alt="badge" />
+            <span :class="`${route.path === '/badge' ? 'text-gray-400' : ''} mt-1 text-xs`">뱃지</span>
+          </router-link>
+          <router-link to="/beaurit-zone" class="flex flex-col items-center justify-center">
+            <img :src="route.path === '/beaurit-zone' ? '/footer/beaurit-zone-gray.png' : '/footer/beaurit-zone-pink.png'" class="w-7 h-7" alt="beaurit-zone" />
+            <span :class="`${route.path === '/beaurit-zone' ? 'text-gray-400' : ''} mt-1 text-xs`">뷰릿존</span>
+          </router-link>
+          <router-link to="/settings" class="flex flex-col items-center justify-center">
+            <img :src="route.path === '/settings' ? '/footer/settings-gray.png' : '/footer/settings-pink.png'" class="w-7 h-7" alt="settings" />
+            <span :class="`${route.path === '/settings' ? 'text-gray-400' : ''} mt-1 text-xs`">설정</span>
+          </router-link>
         </nav>
       </footer>
     </div>

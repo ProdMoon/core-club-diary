c45a9ba5fffae8e0d1186b6687d110409f9ba558
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2024-02-24 05:25:34 +0900

    fix: 일지 화면 디자인 수정

--- src/components/home/TodayAfter.vue
+++ src/components/home/TodayAfter.vue
@@ -1,13 +1,21 @@
 <script setup></script>
 <template>
-  <div
-    class="mt-16 border border-dashed border-pink-500 rounded-2xl w-72 h-24 flex items-center justify-center shadow-lg cursor-pointer hover:bg-pink-50 active:bg-pink-100"
-  >
-    <router-link to="/diary/write" class="w-full h-full flex items-center justify-center">
-      <div class="font-bold text-pink-500">
-        오늘의 일지<br />
-        작성하기 💓
-      </div>
-    </router-link>
+  <div class="w-full h-full flex flex-col items-center">
+    <div class="mt-6 w-full text-right text-xs text-pink-300">
+      <span class="mr-6">2024. 03. 09. 금요일</span>
+    </div>
+    <div class="mt-10 text-center text-sm text-pink-400">
+      코클에 참여한 멋진 당신<br>
+      오늘의 운동은 어떠셨어요?
+    </div>
+    <div
+      class="mt-5 rounded-lg w-80 h-12 flex items-center justify-center shadow-md cursor-pointer bg-pink-400 hover:bg-pink-300 active:bg-pink-500"
+    >
+      <router-link to="/diary/write" class="w-full h-full flex items-center justify-center">
+        <div class="font-bold text-sm text-white">
+          지금 다이어리 작성하기
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
--- src/components/home/TodayBefore.vue
+++ src/components/home/TodayBefore.vue
@@ -5,10 +5,10 @@ import { time2digits } from '@/utils/time.js';
 const leftTimeStore = useLeftTimeStore();
 </script>
 <template>
-  <div class="mt-16 border border-dashed border-pink-500 rounded-2xl w-72 h-40 flex flex-col items-center cursor-pointer">
-    <img class="mt-6" src="/pink-clock.png" alt="pink-clock" />
-    <div class="mt-2 text-pink-400">오후 9시 수업까지</div>
+  <div class="mt-16 border border-dashed border-pink-500 rounded-2xl w-72 h-36 flex flex-col items-center cursor-pointer">
+    <img class="mt-5" src="/pink-clock.png" alt="pink-clock" />
+    <div class="mt-2 text-sm text-pink-400">오후 9시 수업까지</div>
     <div class="mt-1 text-2xl text-pink-400">{{ leftTimeStore.hours }} : {{ time2digits(leftTimeStore.minutes) }} : {{ time2digits(leftTimeStore.seconds) }}</div>
-    <div class="mt-1 text-pink-400">남았어요!</div>
+    <div class="text-sm text-pink-400">남았어요!</div>
   </div>
 </template>
